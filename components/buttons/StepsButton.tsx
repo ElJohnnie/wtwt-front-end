55db62dda4c83d47180d370d241b049ce8e7c8b2
Jonatha Rodrigo Follmer <jonatha.follmer@compasso.com.br>
2024-05-23 17:12:50 -0300

    fix: fix steps button, added text

--- components/buttons/StepsButton.tsx
+++ components/buttons/StepsButton.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 export interface StepsButtonProps {
   icon: JSX.Element;
   callback?: () => void;
+  text?: string;
 }
 
 export default function StepsButton(props: StepsButtonProps) {
-  const { icon, callback } = props;
+  const { icon, callback, text } = props;
   return (
-    <div className="flex flex-row justify-center items-center">
+    <div className="w-full flex flex-col justify-center items-center">
       <button
         className="w-24 h-24 rounded-full  
           inline-flex items-center justify-center  
@@ -17,6 +18,7 @@ export default function StepsButton(props: StepsButtonProps) {
       >
         {icon}
       </button>
+      <p className="text-[18px] mt-1 text-pink-600">{text}</p>
     </div>
   );
 }
--- stories/buttons/StepsButton.stories.tsx
+++ stories/buttons/StepsButton.stories.tsx
@@ -31,6 +31,27 @@ ArrowIcon.args = {
   ),
 };
 
+export const WithText = Template.bind({});
+WithText.args = {
+  icon: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="size-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+      />
+    </svg>
+  ),
+  text: 'Próximo',
+};
+
 export const RandonIcon = Template.bind({});
 RandonIcon.args = {
   icon: (
