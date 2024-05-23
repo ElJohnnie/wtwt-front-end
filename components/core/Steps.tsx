9bf5caa9ec9c4617e2571e40ac3bc3d029b8b96b
Jonatha Rodrigo Follmer <jonatha.follmer@compasso.com.br>
2024-05-23 13:37:16 -0300

    feat: make steps component

--- components/core/Steps.tsx
+++ components/core/Steps.tsx
@@ -1,10 +1,38 @@
-import 'stylesSteps.modules.css';
+import React from 'react';
 
-interface StepsProps {
+export interface StepsProps {
   number: number;
-  color: string;
+  hasRange: boolean;
+  isEmpty?: boolean;
+  asComplete?: boolean;
+  isInactive: boolean;
 }
 
 export default function Steps(props: StepsProps) {
-  return <div></div>;
+  const { number, hasRange, asComplete, isEmpty, isInactive } = props;
+  return (
+    <div className="flex flex-row justify-center items-center">
+      <div
+        className={`w-24 h-24 rounded-full  
+          inline-flex items-center justify-center  
+          text-white text-[24px] font-bold ${
+            isInactive ? 'bg-pink-400' : 'bg-pink-600'
+          }`}
+      >
+        {number}
+      </div>
+      {hasRange && (
+        <div className="w-36 bg-gray-200 rounded-full h-2.5 ml-4 dark:bg-gray-700">
+          <div
+            className="bg-pink-600 h-2.5 rounded-full"
+            style={
+              !isEmpty && !isInactive
+                ? { width: asComplete ? '100%' : '50%' }
+                : { width: '0%' }
+            }
+          ></div>
+        </div>
+      )}
+    </div>
+  );
 }
