f390e17da7eebe50aa241753829283fb72cb3aa6
Jesschuck <jaayschuck@gmail.com>
2024-05-25 21:59:38 -0300

    wip: image component wip

--- components/images/ImageComponent.tsx
+++ components/images/ImageComponent.tsx
@@ -3,18 +3,29 @@ import Image from 'next/image';
 
 export interface ImageComponentProps {
   href: string;
+  title: string;
+  description: string;
 }
 
 export default function ImageComponent(props: ImageComponentProps) {
-  const { href } = props;
+  const { href, title, description } = props;
   return (
-    <figure className="max-w-lg">
-      <Image
-        layout="fill"
-        objectFit="contain"
-        src={href}
-        alt="image description"
-      />
-    </figure>
+    <div className="flex flex-col items-center mx-auto max-w-sm">
+      <figure className="relative w-full h-64">
+        <Image
+          layout="fill"
+          objectFit="contain"
+          src={href}
+          alt="image description"
+          className="absolute inset-0"
+        />
+      </figure>
+      <h3 className="font-bold text-pink-600 text-center text-lg mt-3 max-w-x">
+        {title}
+      </h3>
+      <p className="text-pink-600 text-center text-sm mt-2 max-w-sm text-justify">
+        {description}
+      </p>
+    </div>
   );
 }
--- stories/images/ImageComponent.stories.tsx
+++ stories/images/ImageComponent.stories.tsx
@@ -16,4 +16,7 @@ const Template: StoryFn<ImageComponentProps> = (args) => (
 export const Default = Template.bind({});
 Default.args = {
   href: 'https://image.tmdb.org/t/p/w500/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg',
+  title: 'Clube da Luta',
+  description:
+    'Um homem deprimido que sofre de insônia conhece um estranho vendedor chamado Tyler Durden e se vê morando em uma casa suja depois que seu perfeito apartamento é destruído. A dupla forma um clube com regras rígidas onde homens lutam. A parceria perfeita é comprometida quando uma mulher, Marla, atrai a atenção de Tyler.',
 };
