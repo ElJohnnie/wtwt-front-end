ee9242b37ac82807b6eac4a5b949f487f199391b
Jonatha <jonatha.follmer@telefonicati.onmicrosoft.com>
2024-06-17 17:58:17 -0300

    fix: fix steps responsivity

--- components/core/Steps.tsx
+++ components/core/Steps.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export interface StepsProps {
-  number: number;
+  number: string;
   hasRange: boolean;
   isEmpty?: boolean;
   asComplete?: boolean;
@@ -13,7 +13,7 @@ export default function Steps(props: StepsProps) {
   return (
     <div className="flex flex-row justify-center items-center">
       <div
-        className={`w-24 h-24 rounded-full  
+        className={`size-10 md:size-16 xl:size-24 rounded-full  
           inline-flex items-center justify-center  
           text-white text-[24px] font-bold ${
             isInactive ? 'bg-pink-400' : 'bg-pink-600'
@@ -22,7 +22,7 @@ export default function Steps(props: StepsProps) {
         {number}
       </div>
       {hasRange && (
-        <div className="w-36 bg-gray-200 rounded-full h-2.5 ml-4 dark:bg-gray-700">
+        <div className="w-8 lg:w-36 md:w-16 bg-gray-200 rounded-full invisible md:visible mx-0 h-2.5 md:mx-4 dark:bg-gray-700">
           <div
             className="bg-pink-600 h-2.5 rounded-full"
             style={
