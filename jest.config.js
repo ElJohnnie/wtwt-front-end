f3b0cd7983244f3274efb5a054767dfaab1a4532
Jonatha <jonatha.follmer@telefonicati.onmicrosoft.com>
2024-07-18 16:26:41 -0300

    fix: fix jest configs

--- jest.config.js
+++ jest.config.js
@@ -16,6 +16,9 @@ const customJestConfig = {
   collectCoverageFrom: [
     'app/**/*.{js,jsx,ts,tsx}',
     'components/**/*.{js,jsx,ts,tsx}',
+    'contexts/**/*.{js,jsx,ts,tsx}',
+    'hooks/**/*.{js,jsx,ts,tsx}',
+    'services/**/*.{js,jsx,ts,tsx}',
     '!**/*.d.ts',
     '!**/node_modules/**',
     '!**/vendor/**',
