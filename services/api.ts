cb0e104bec5a10af2b1118deb689cab283718a7a
Jonatha <jonatha.follmer@telefonicati.onmicrosoft.com>
2025-03-15 15:16:11 -0300

    change api code for docker testing

--- services/api.ts
+++ services/api.ts
@@ -1,25 +1,27 @@
 import axios from 'axios';
 
-export const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-  timeout: Infinity,
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_AUTHORIZATION_TOKEN}`,
-  },
-});
-
 const fetchData = async (
   url: string,
   queryParams: Record<string, unknown> = {},
 ) => {
   try {
-    console.log(axiosInstance.defaults.baseURL);
-    const response = await axiosInstance.get(url, {
-      params: queryParams,
-    });
+    console.log('Base URL:', process.env.NEXT_PUBLIC_API_URL);
+    console.log('Full URL:', `${process.env.NEXT_PUBLIC_API_URL}${url}`);
+
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}${url}`,
+      {
+        params: queryParams,
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_AUTHORIZATION_TOKEN}`,
+        },
+      },
+    );
+
     return response.data;
   } catch (error) {
+    console.error('Erro na requisição:', error);
     throw new Error('Could not get data');
   }
 };
